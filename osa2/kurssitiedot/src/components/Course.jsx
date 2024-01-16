99d4e245263febe2403ee947ffc58482f6be1f5d
Anna-Reetta Julkunen <ar.julkunen@gmail.com>
2024-01-16 13:16:51 +0200

    Exercises 2.2/2.3 kurssitiedot step 7 and 8

--- osa2/kurssitiedot/src/components/Course.jsx
+++ osa2/kurssitiedot/src/components/Course.jsx
@@ -20,12 +20,22 @@ const Content = ({ parts }) => {
   );  
 }
 
+const Total = ({ parts }) => {
+  const total = parts.reduce( (sum, part) => {
+    return sum + part.exercises
+  }, 0);
+
+  return (
+    <h4>total of { total } exercises</h4>
+  );
+}
 
 const Course = ({ course }) => {
     return (
         <div>
           <Header course={ course } />
-          <Content parts={ course.parts } /> 
+          <Content parts={ course.parts } />
+          <Total parts={ course.parts } />
         </div>
       );
 }
